e209312ec0bd321cc5353dfa1ed0f84c7bd9a95f
Markus Johansson <markus.johansson@1928diagnostics.com>
2021-04-15 14:05:18 +0200

    Added script to backup gene panels from scout

---
+++ scripts/backup_gene_panel.sh
@@ -0,0 +1,2 @@
+ssh cmdscout2 docker-compose -f scout_stack/docker-compose.prod.yml exec -T mongodb mongoexport --host mongodb --db scout --collection gene_panel --jsonArray | ssh rs-fs1 "cat > /fs1/resources/scout/gene_panel/scout_gene_panel__$(date --iso-8601).json"
+ssh rs-fs1 ln -s "/fs1/resources/scout/gene_panel/scout_gene_panel__$(date --iso-8601).json" "/fs1/resources/scout/scout_gene_panel__latest.json"
